876dc75e646cf365a1ae3b6937a3a7cd8fd88d3d
digitalemerge <arturo.opsetmoen@amador.no>
2022-02-28 19:27:09 +0100

    fix ingestion script

--- ingestion.py
+++ ingestion.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
+from pathlib import Path
+from typing import Union
 import json
 from datetime import datetime
-
-
 
 
 #############Load config.json and get input and output paths
@@ -15,12 +15,21 @@ input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
 
+def merge_multiple_dataframe(input_path: Union[str, Path]) -> pd.DataFrame:
+    """
+    Function to read all csv files in the input_path and merge the data into a single dataframe.
+    The data files should have the same schema.
+    :param input_path: Path object from which the csv files will be ingested.
+    :return: Pandas dataframe object.
+    """
+    csv_paths = Path(input_path).rglob('*.csv')
+    data_frame_tot = pd.DataFrame()
+    for path in csv_paths:
+        data_frame_temp = pd.read_csv(path)
+        data_frame_tot = pd.concat([data_frame_tot, data_frame_temp], ignore_index=True)
 
-#############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
-
+    return data_frame_tot
 
 
 if __name__ == '__main__':
-    merge_multiple_dataframe()
+    merge_multiple_dataframe(input_folder_path)
